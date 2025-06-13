252aa256093f5d2f49b2cea8e67da3722e4d5687
Poriro123 <eljuliusbelmont@gmail.com>
2025-06-13 13:53:28 -0300

    Probando incluir busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
 import FrmRegistar from "./components/FrmRegistrar";
+import React from 'react';
+import Search from './components/Search';
+import initialDetails from './data/initialDetails';
 
 function App() {
   return <FrmRegistar />;
 }
-
+function App() {
+  return (
+    <div className="tc bg-green ma0 pa4 min-vh-100">
+      <Search details={initialDetails}/>
+    </div>
+  );
+}
 export default App;
