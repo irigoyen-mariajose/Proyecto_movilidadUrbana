50aa3285a92862a7aa11cc5828ff044a42ae96b2
lepe-altf4 <lepemate1310@gmail.com>
2025-06-17 17:26:09 -0300

    css del formulario de inicio sesion

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import React from "react";
 import Search from "./components/Search";
 import initialDetails from "./components/initialDetails";
 import Navbar from "./components/NavbarBARRA";
+import FrmIniciosesion from "./components/FrmIniciosesion";
+
 
 function App() {
   return (
     <div className="tc bg-green ma0 pa4 min-vh-100">
       <Navbar />
       <Search details={initialDetails} />
+      <div className="contenedor-formularios">
       <FrmRegistrar />
+      <FrmIniciosesion />
+      </div>    
     </div>
   );
 }
--- src/components/FrmIniciosesion.jsx
+++ src/components/FrmIniciosesion.jsx
@@ -1,46 +1,76 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/FormularioNombreApellido.css";
-import "../css/App.css";
+import "../css/FrmIniciosesion.css";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const FrmIniciosesion = ({ titulo = "Iniciar sesion" }) => {
   const [correo, setCorreo] = useState("");
-  const [contraseña, setContrasenia] = useState("");
+  const [contrasenia, setContrasenia] = useState("");
+  const [mostrarContrasenia, setMostrarContrasenia] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     navigate("/resultado", {
-      state: { correo, contraseña },
+      state: { correo, contrasenia },
     });
   };
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="glass-form">
-        <h2>{titulo}</h2>
+        <h2 className="titulo">{titulo}</h2>
 
         <div className="form-group">
-          <label>Correo</label>
+          <label></label>
           <input
-            type="text"
+          className="barras"
+            type="email"
             value={correo}
             onChange={(e) => setCorreo(e.target.value)}
-            placeholder="Escribe tu correo"
+            placeholder="Escribe tu correo" required
           />
         </div>
-        <div className="form-group">
-          <label>Contraseña</label>
+        <div className="form-group password-wrapper">
+          <label></label>
           <input
-            type="text"
-            value={contraseña}
+            className="barras"
+            type={mostrarContrasenia ? "text" : "password"}
+            value={contrasenia}
             onChange={(e) => setContrasenia(e.target.value)}
-            placeholder="Ingrese su contraseña"
+            placeholder="Contraseña" required
+            style={{ paddingRight: "40px" }}
           />
+          <span
+            className="ojo-icon-password"
+            onClick={() => setMostrarContrasenia(!mostrarContrasenia)}
+          >
+            {mostrarContrasenia ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </span>
         </div>
 
-        <button type="submit">Entrar</button>
+        <button type="submit"
+        className="button">Entrar</button>
+      <div className="social-login">
+  <p className="texto-social">O iniciá sesión con:</p>
+  <div className="botones-rds-sociales">
+    <button type="button" className="btn-google">
+      <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google" />
+      Google
+    </button>
+    <button type="button" className="btn-facebook">
+      <img src="https://img.icons8.com/fluency/16/000000/facebook-new.png" alt="Facebook" />
+      Facebook
+    </button>
+  </div>
+</div>
+
+        <p className="texto-opcion">
+          ¿No tenés cuenta, burro? <span className="enlace-regitrarse">Registrate</span>
+        </p>
       </form>
     </div>
   );
