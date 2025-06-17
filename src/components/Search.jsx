2b844e4f9e60b1881e76e7e24fff33204834bf57
Angelesrojas <angelesrojaaz[200~@ git config --global user.email angelesrojaaz@gmail.com>
2025-06-17 18:39:04 -0300

    Borrando y modificando busqueda

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Scroll from './Scroll';
-import SearchList from './SearchList';
+
 
 function Search({ details }) {
 
@@ -25,28 +25,20 @@ function Search({ details }) {
     setSearchField(e.target.value);
   };
 
-  function searchList() {
-    return (
-      <Scroll>
-        <SearchList filteredPersons={filteredPersons} />
-      </Scroll>
-    );
-  }
 
   return (
     <section className="garamond">
-      <div className="navy georgia ma0 grow">
-        <h2 className="f2">Search your course</h2>
+      <div className="navy georgia ma0 grow ">
+        <h2 className="f2 light-yellow">Busqueda del mapa</h2>
       </div>
       <div className="pa2">
         <input 
-          className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+          className="pa3 bb br3 grow b--none bg-light-yellow ma3"
           type = "search" 
-          placeholder = "Search People" 
+          placeholder = "¿A donde vas?" 
           onChange = {handleChange}
         />
       </div>
-      {searchList()}
     </section>
   );
 }
