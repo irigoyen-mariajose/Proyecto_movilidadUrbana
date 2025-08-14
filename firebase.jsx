fea68e6eb7283bfda1bb8d5d7648ebc0d06e7bde
Poriro123 <eljuliusbelmont@gmail.com>
2025-08-14 20:22:11 -0300

    Ignorando archivo

--- firebase.jsx
+++ firebase.jsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_i7dCZfPIxal9r1aWIteary9p_pN__DY",
-  authDomain: "movilidad-urbana-57613.firebaseapp.com",
-  projectId: "movilidad-urbana-57613",
-  storageBucket: "movilidad-urbana-57613.firebasestorage.app",
-  messagingSenderId: "358102677215",
-  appId: "1:358102677215:web:5951f38e9b629313cf9b6a",
-  measurementId: "G-3BXRKDEWCN"
+  apiKey: "AIzaSyARQKcFihBTEvFnIZNpf5tlg4jyNiMqvNY",
+  authDomain: "andesur-db250.firebaseapp.com",
+  projectId: "andesur-db250",
+  storageBucket: "andesur-db250.firebasestorage.app",
+  messagingSenderId: "820374944627",
+  appId: "1:820374944627:web:cc4fc380cce03accfda306",
+  measurementId: "G-4BBBLNBRVH"
 };
 
 // Initialize Firebase
