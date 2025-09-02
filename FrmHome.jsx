11a224a2ddd1497f1e6a158f1bb83bfc3bf26ed8
aixaa <aixarochaepet20@gmail.com>
2025-09-02 15:49:40 -0300

    Add files via upload

---
+++ FrmHome.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Navbar from "./NavbarBARRA";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import "../css/FrmIniciosesion.css";
+import FrmRegistar from "./FrmRegistrar";
+import "../css/FrmRegistrar.css";
+
+function Inicio (){
+    return (
+    <div ClaseHome="Home" >
+        <h1>Viaja con Andensur por todo el valle de Neuquen</h1>
+            <div>
+                <link to="/FrmRegistrar"> Registrarme →</link>
+                <link to="/FrmInicioSesion">Iniciar sesion→</link>
+            </div>
+    </div>
+);
+}
+
+export default Inicio;
